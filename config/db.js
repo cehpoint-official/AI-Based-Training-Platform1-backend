cc6bf176eab16b21d61d8e3a7ddfe5fc5234db2e
EC2 Default User <ec2-user@ip-172-31-13-178.ap-south-1.compute.internal>
2024-12-29 14:07:07 +0000

    Some Changes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI,{
+       useNewUrlParser: true,
+      useUnifiedTopology: true,
+       
+});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
